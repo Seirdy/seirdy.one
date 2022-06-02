425071eade1b458a8401ef6b0d62e9952ddebd54
Rohan Kumar <seirdy@seirdy.one>
2022-06-01 17:30:47 -0700

    Auto-format markup in CI
    Use xmlfmt with some extra post-processing since xmlfmt has a few
    issues wrt extra newlines.

--- scripts/xhtmlize.sh
+++ scripts/xhtmlize.sh
@@ -5,7 +5,8 @@
 # XML tooling.
 # this means that every index.html file has an equivalent index.xhtml file.
 # content negotiation allows an agent to pick html or xhtml.
+# echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 
 find "$1" -type f -name '*.html' \
-	-exec sh -c 'echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" | cat - "$0" | grep -Fv "<meta charset=\"UTF-8\" />" >"${0%*.html}.xhtml"' {} \;
+	-exec sh -c 'echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >"${0%*.html}.xhtml" && xmlfmt -i "	" -f "$0" | busybox sed -e :a -e "/./,\$!d;/^\n*\$/{\$d;N;};/\n\$/ba" | sd -f m "\n(?:\t*)?</" "</" | sd -f m "<pre>\n\t*<code" "<pre><code" | pee "sed 5d >>\"${0%*.html}.xhtml\"" "sponge \"$0\""' {} \;
 # done
