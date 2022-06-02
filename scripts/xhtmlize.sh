89139337d1408b61c461a4a746013f9913ae3286
Rohan Kumar <seirdy@seirdy.one>
2022-06-01 21:00:19 -0700

    Fix some whitespace

--- scripts/xhtmlize.sh
+++ scripts/xhtmlize.sh
@@ -8,5 +8,5 @@
 # echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 
 find "$1" -type f -name '*.html' \
-	-exec sh -c 'echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >"${0%*.html}.xhtml" && xmlfmt -i "	" -f "$0" | busybox sed -e :a -e "/./,\$!d;/^\n*\$/{\$d;N;};/\n\$/ba" | sd -f m "\n(?:\t*)?</" "</" | sd -f m "<pre>\n\t*<code" "<pre><code" | pee "sed 5d >>\"${0%*.html}.xhtml\"" "sponge \"$0\""' {} \;
+	-exec sh -c 'echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >"${0%*.html}.xhtml" && xmlfmt -i "	" -f "$0" | busybox sed -e :a -e "/./,\$!d;/^\n*\$/{\$d;N;};/\n\$/ba" | sd -f m "\n(?:\t*)?</" "</" | sd -f m "<pre>\n\t*<code" "<pre><code" | sd -f m "\(\n\t*<abbr" "(<abbr" | pee "sed 5d >>\"${0%*.html}.xhtml\"" "sponge \"$0\""' {} \;
 # done
