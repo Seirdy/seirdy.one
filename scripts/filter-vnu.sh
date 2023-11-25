c0f7318090a5c1b7700cabae5a0248c0b1c605e5
Rohan Kumar <seirdy@seirdy.one>
2023-11-25 13:41:35 -0800

    Trivial style fix

--- scripts/filter-vnu.sh
+++ scripts/filter-vnu.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # filters the Nu HTML Validator's JSON output. Operates on stdin.
+# no pipefail here since there are no pipes.
 set -e -u
 
 root_dir="$(dirname "$0")/.."
 vnu_output="$(jaq --from-file "$root_dir/linter-configs/vnu_filter.jq")"
 if [ "$vnu_output" = '' ]; then
 	echo "All markup is valid"
-else
-	echo "Markup errors found!" 1>&2
-	echo "$vnu_output"
-	exit 1
+	exit 0
 fi
+echo "Markup errors found!" 1>&2
+echo "$vnu_output"
+exit 1
