074620f70e1ef1d98c465135aa5af1495a0a24c7
Rohan Kumar <seirdy@seirdy.one>
2023-11-25 12:59:38 -0800

    Support custom timeout

--- scripts/curl-wrapper.sh
+++ scripts/curl-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # a curl wrapper for seirdy.one
-curl --proto "=https" --tlsv1.3 --cert-status -sS -m10 "$@"
+curl --proto "=https" --tlsv1.3 --cert-status -sS -m${SEIRDYONE_CURL_TIMEOUT-10} "$@"
