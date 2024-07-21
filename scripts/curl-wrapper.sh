f07aa88c6c6cf5a88df21f1d0fec5f8b82b87dc6
Seirdy <seirdy@seirdy.one>
2024-07-20 22:12:12 -0400

    Remove OCSP check on seirdy.one (my ocsp broke 😭)

--- scripts/curl-wrapper.sh
+++ scripts/curl-wrapper.sh
@@ -3,4 +3,4 @@
 # no pipefail here since there are no pipes.
 
 set -e -u
-curl --proto "=https" --tlsv1.3 --cert-status -sS -m"${SEIRDYONE_CURL_TIMEOUT-10}" "$@"
+curl --proto "=https" --tlsv1.3 -sS -m"${SEIRDYONE_CURL_TIMEOUT-10}" "$@"
